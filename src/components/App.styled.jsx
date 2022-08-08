72b1f6fd56604f9ab0afd1d29de898b2684bd7ed
Oksana Semko <osemko@gmail.com>
2022-08-08 10:56:36 +0300

    Added styles to App

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  color: #010101;
+`;
